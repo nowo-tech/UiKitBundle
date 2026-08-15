d00e528cf2283c185a3d35ac5e440b14e2640f73
GitHub Action <action@github.com>
2026-08-15 17:05:49 +0000

    Apply PHP CS Fixer fixes [skip ci]

--- src/DependencyInjection/NowoUiKitExtension.php
+++ src/DependencyInjection/NowoUiKitExtension.php
@@ -61,8 +61,6 @@ final class NowoUiKitExtension extends Extension implements PrependExtensionInte
 
     /**
      * @param array<int, array<string, mixed>> $configs
-     *
-     * @return void
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
